a46c93aff215786bbbac5c204b9658ffb93c9210
Juarez Martins <juniorbmx13@gmail.com>
2023-06-01 21:42:32 -0300

    feat: criação classe Seller, relação com classe Department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,19 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");  //teste
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Juca", "jucamartins@email.com", new Date(), 2300.0,
+                obj);
+
+
+        System.out.println(seller);
 
     }
 }
